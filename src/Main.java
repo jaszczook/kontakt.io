a61426e720520836df2d908f6dbca0a15f063249
Kuba <jaszczook@gmail.com>
2023-05-03 21:23:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        String fileName = "hosts_access_log_00.txt";
+        Set<String> uniqueGifFilenames = getUniqueGifFilenames(fileName);
+        writeToGifsFile(fileName, uniqueGifFilenames);
+    }
+
+    private static Set<String> getUniqueGifFilenames(String fileName) {
+        final String get = "GET";
+        final String gif = "gif";
+        final Set<String> gifFilenames = new HashSet<>();
+
+        try (Stream<String> linesStream = Files.lines(Paths.get(fileName))) {
+            linesStream.forEach(line -> {
+                Pattern quotePattern = Pattern.compile("\"([^\"]*)\"");
+                Pattern httpStatusPattern = Pattern.compile("\\s(\\d{3})\\s+\\d+$");
+                Matcher quoteMatcher = quotePattern.matcher(line);
+                Matcher httpStatusMatcher = httpStatusPattern.matcher(line);
+
+                if (quoteMatcher.find() && httpStatusMatcher.find()) {
+                    String request = quoteMatcher.group(1);
+                    String[] requestParts = request.split("\\s+");
+                    int httpStatusCode = Integer.parseInt(httpStatusMatcher.group(1));
+                    File file = new File(requestParts[1]);
+                    String filename = file.getName();
+                    int dotIndex = filename.lastIndexOf('.');
+
+                    if (get.equals(requestParts[0]) && // is a GET request
+                            httpStatusCode == 200 && // with HTTP status code 200
+                            dotIndex > 0 && // filename contains '.'
+                            gif.equalsIgnoreCase(filename.substring(dotIndex + 1)) // file extension is 'gif' case ignored
+                    ) {
+                        gifFilenames.add(filename);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read file.");
+        }
+
+        return gifFilenames;
+    }
+
+    private static void writeToGifsFile(String fileName, Set<String> uniqueGifFilenames) {
+        Path filePath = Paths.get("gifs_" + fileName);
+        try {
+            Files.write(filePath, uniqueGifFilenames);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write file.");
+        }
+    }
+}
